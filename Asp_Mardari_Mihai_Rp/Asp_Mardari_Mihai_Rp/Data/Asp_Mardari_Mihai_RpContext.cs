5e8550c11e4446e21855d32d7b6ff1a0c0f07779
mike97m <mike9m7@yahoo.com>
2020-05-05 15:32:00 +0300

    Lab 10 .NET Core Razor

---
+++ Asp_Mardari_Mihai_Rp/Asp_Mardari_Mihai_Rp/Data/Asp_Mardari_Mihai_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Mardari_Mihai_Rp.Models;
+
+namespace Asp_Mardari_Mihai_Rp.Data
+{
+    public class Asp_Mardari_Mihai_RpContext : DbContext
+    {
+        public Asp_Mardari_Mihai_RpContext (DbContextOptions<Asp_Mardari_Mihai_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Mardari_Mihai_Rp.Models.Movie> Movie { get; set; }
+    }
+}
