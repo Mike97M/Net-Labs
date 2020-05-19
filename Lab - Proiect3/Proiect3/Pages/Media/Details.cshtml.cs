c9db194d541bd7d2caa20c78ed64a81f6fcf3330
mike97m <mike9m7@yahoo.com>
2020-05-19 15:32:21 +0300

    lab saptaman asta

---
+++ Lab - Proiect3/Proiect3/Pages/Media/Details.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceReferenceMedia;
+namespace Proiect3.Pages.Media
+{
+    public class DetailsModel : PageModel
+    {
+        PostMediaClient client = new PostMediaClient();
+        public Medias Media { get; set; }
+        public DetailsModel()
+        {
+            Media = new Medias();
+        }
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            Media = await client.GetMediaByIdAsync(id.Value);
+            if (Media == null)
+                return NotFound();
+            return Page();
+        }
+    }
+}
---
+++ Lab - Proiect3/Proiect3/Pages/Media/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceReferenceMedia;
+
+namespace Proiect3.Pages.Media
+{
+    public class IndexModel : PageModel
+    {
+        PostMediaClient client = new PostMediaClient();
+        public List<Medias> MediaList { get; set; }
+        public IndexModel()
+        {
+            this.MediaList = new List<Medias>();
+        }
+        public async Task OnGetAsync()
+        {
+            var medias = await client.GetAllPhotosAsync();
+            this.MediaList = medias.ToList<Medias>();
+        }
+    }
+}
